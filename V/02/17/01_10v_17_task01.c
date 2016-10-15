195718bfcd20f95b3316d43e4fa103d2d1c83a43
Wrobel8 <malinov.marti@gmail.com>
2016-10-15 22:24:17 +0300

    Update 01_10v_17_task01.c

--- V/02/17/01_10v_17_task01.c
+++ V/02/17/01_10v_17_task01.c
@@ -2,12 +2,11 @@
 
 int main() {
 	int a;
-	printf("\n Input number > ");
 	scanf("%d" , &a);
 	int m = a;
 	a = a*a;
 	printf("\n%d" , a);
 	a = a*m;
-	printf("\n%d\n" , a);
+	printf("\n%d" , a);
 }
 
